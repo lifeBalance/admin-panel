ddaa7ec8908f5cd202cffee006daa5b247711560
javi <3051496+lifeBalance@users.noreply.github.com>
2023-04-03 11:18:52 +0300

    add chart to dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,51 @@
 import Wrapper from '../components/Wrapper'
+import * as c3 from 'c3'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Dashboard = () => {
+  useEffect(() => {
+    (async () => {
+      const chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [
+            ['x'],
+            ['Sales'],
+          ],
+          types: {
+            Sales: 'bar',
+          },
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d',
+            },
+          },
+        },
+      })
+
+      const response = await axios.get('chart')
+      console.log(response)
+      chart.load({
+        columns: [
+          [
+            'x',
+            ...response.data.result.map((r: { date: string, sum: string }) => new Date(r.date)),
+          ],
+          ['Sales', ...response.data.result.map((r: { date: string, sum: string }) => +r.sum)],
+        ],
+      })
+    })()
+  }, [])
+
   return (
     <Wrapper>
-      <div>Dashboard</div>
+      <h2>Daily Sales</h2>
+      <div id='chart'></div>
     </Wrapper>
   )
 }
